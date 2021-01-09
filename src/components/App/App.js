147eb8db9cbb693a3dae661670f5a967dd7c1aa5
Анастасия Антонова <antonovanastya11@gmail.com>
2021-01-10 00:08:44 +0300

    props

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import InputItem from '../InputItem/InputItem'
 import ItemList from '../ItemList/ItemList'
+import Footer from '../Footer/Footer'
 
-
+const todoItem = 'Написать новое приложение';
 const App = () => (
   <div>
     <h1>Важные дела:</h1>
-    <ItemList />
+    <InputItem />
+      <ItemList todoItem={todoItem}/>
+      <Footer count ={3} />
+
+
   </div>);
 
   export default App;
