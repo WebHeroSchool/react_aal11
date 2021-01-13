a51a4ec74ff724375cb2acab09ceffd1f9c5d3ee
Анастасия Антонова <antonovanastya11@gmail.com>
2021-01-14 00:17:06 +0300

    вывод списка элементов и доб.ключа

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,24 @@ import ItemList from '../ItemList/ItemList'
 import Footer from '../Footer/Footer'
 
 const todoItem = 'Написать новое приложение';
-const App = () => (
-  <div>
-    <h1>Важные дела:</h1>
-    <InputItem />
-      <ItemList todoItem={todoItem}/>
-      <Footer count ={3} />
-
-
-  </div>);
-
+const App = () => {
+	  const items = [
+      {
+          value: 'Написать новое приложение'
+      }, 
+      { 
+          value: 'Выполнить +2 задания'
+      },
+      {
+          value: 'Сводить кота в ветеренарную клинику'
+      }
+    ];
+    return (
+      <div>
+        <h1>Важные дела:</h1>
+        <InputItem />
+        <ItemList items={items}/>
+        <Footer count ={3} />
+      </div>);
+}
   export default App;
