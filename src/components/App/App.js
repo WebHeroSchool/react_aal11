7b817ccb7144e9dc27e82e66f9c628729f687d8b
Анастасия Антонова <antonovanastya11@gmail.com>
2021-01-20 21:22:42 +0300

    state

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,26 +5,29 @@ import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
 class App extends React.Component {
-    render() {
-        const items = [
-        {
-            value: 'Написать новое приложение',
-            isDone: false
-        },
-        {
-            value: 'Пройти 1ур по react +10стр',
-            isDone: true
-        },
-        {
-            value: 'Сходить на трен-ку в 20:00 ',
-            isDone: false
-        }
-    ];
-    return (
+    state = {
+        items: [
+          {
+              value: 'Написать новое приложение',
+              isDone: false
+          },
+          {
+              value: 'Пройти 1ур по react +10стр',
+              isDone: true
+          },
+          {
+              value: 'Сходить на трен-ку в 20:00 ',
+              isDone: false
+          }
+      ]
+  };
+  
+  render() {     
+    return(
         <div className={styles.wrap}>
             <h1 className={styles.title}>Важные дела:</h1>
                 <InputItem/>
-                <ItemList items={items} />
+                <ItemList items={this.state.items} />
                 <Footer count={3}/>
         </div>);
   }
